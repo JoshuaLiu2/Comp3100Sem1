6d9dcbb89f3003649f6d8bd194bd73faa05d4f39
Josh <joshua.liu2@students.mq.edu.au>
2023-06-06 03:21:33 +1000

    as2 client progress - data command

--- Week12/A2/MyClient.java
+++ Week12/A2/MyClient.java
@@ -67,14 +67,24 @@ System.out.println("-------------");
 String currentJob = strArr[0];
 //System.out.println(currentJob);
 
+//variables used for scheduling jobs
+int jobID = -1;
+int jobCore = 0;
+String serverID = "";
+int serverCore = 0;
+String serverName = "";
 
 //Loop scheduling of jobs
 while (!currentJob.equals("NONE")) {
-	System.out.println("inside while loop");
+	//System.out.println("inside while loop");
 	//System.out.println("Current job: " + currentJob);
 	if(currentJob.equals("JOBN")) {
 		System.out.println("inside jobn");
-		
+		//set cores required for job 
+		jobCore = Integer.parseInt(strArr[4]);
+		jobID++;
+		System.out.println("JobCores = " + jobCore);
+		System.out.println("JobID = " + jobID);
 		inputString = "GETS Capable " + strArr[4] +" " + strArr[5] +" " + strArr[6];
 		//Reply to job
 		System.out.println("Job reply to server: " + inputString);
@@ -93,41 +103,93 @@ while (!currentJob.equals("NONE")) {
 		
 		int noRecords = Integer.parseInt(strArr[1]);
 		int recLength = Integer.parseInt(strArr[2]);
-		System.out.println("-----");
-		System.out.println(noRecords);
-		System.out.println(recLength);
-			
+		
+
+		//write out all available servers
+		for(int i = 0; i < noRecords; i++) {
+		//Print each seperate line for available servers
 		inputString = "OK";
 		System.out.println("Job reply to server: " + inputString);
 		dout.write((inputString+"\n").getBytes()); 
 		dout.flush();
-		for(int i = 0; i < noRecords; i++) {
-		//first fit
-			if (i = 0) {
-			
-			System.out.println(i);
-			outputString=in.readLine();  
-			System.out.println("Server says: "+outputString); 
+		System.out.println("-----Loop: " + i + "-----");
+		outputString=in.readLine();  
+		System.out.println("Server says: "+outputString); 
+		strArr = outputString.split("\\s+");
+		
+		//Determining individual cores and server id and names
+		serverCore = Integer.parseInt(strArr[4]);
+		//String cores = strArr[4];
+		//System.out.println("This server is: " + strArr[1] + "." + strArr[0]);
+		//System.out.println("This server has: " + cores + " cores.");
+		System.out.println("This server has: " + serverCore + "cores");
+		
+		//Determining best fit
+		int bestFitValue = 0;
+		int comparisonValue = 0;
+		//initialise bf to first server
+		if (i == 0) {
+			bestFitValue = serverCore - jobCore;
+			serverName = strArr[0];
+			serverID = strArr[1];
+		}
+		 
+		int compare = serverCore - jobCore;
+		comparisonValue = Math.abs(compare);
+		System.out.println("Best Fit Value to beat: " + bestFitValue);
+		System.out.println("Current Best Fit Value: " + comparisonValue);
+
+		//check if next server is better fit
+		if (comparisonValue < bestFitValue) {
+			serverName = strArr[0];
+			serverID = strArr[1];
+			bestFitValue = comparisonValue;
+			}
 		}
  		inputString = "OK";
 		System.out.println("Job reply to server: " + inputString);
 		dout.write((inputString+"\n").getBytes()); 
 		dout.flush();
-
+		outputString=in.readLine();  
+		System.out.println("Server says: "+outputString); 
 		strArr = outputString.split("\\s+");
 		currentJob = strArr[0];
-		currentJob = "NONE";
 		
-		//inputString = "OK";
-		//System.out.println("Job reply to server: " + inputString);
-		//dout.write((inputString+"\n").getBytes()); 
-		//dout.flush();
+	}
+	if(currentJob.equals("JCPL")) {
+		inputString = "REDY";
+		System.out.println("Job reply to server: " + inputString);
+		dout.write((inputString+"\n").getBytes()); 
+		dout.flush();
 		outputString=in.readLine();  
-		System.out.println("Server says: "+outputString);  
+		System.out.println("Server says: "+outputString); 
 		
+		strArr = outputString.split("\\s+");
+		currentJob = strArr[0];
+	}
+	if(currentJob.equals(".")) {
+		inputString = "SCHD " + jobID + " " + serverName + " " + serverID;
+		System.out.println("Job reply to server: " + inputString);
+		dout.write((inputString+"\n").getBytes()); 
+		dout.flush();
+		outputString=in.readLine();  
+		System.out.println("Server says: "+outputString);
+
+		inputString = "OK";
+		System.out.println("Job reply to server: " + inputString);
+		dout.write((inputString+"\n").getBytes()); 
+		dout.flush();
+		outputString=in.readLine();  
+		System.out.println("Server says: "+outputString); 
+		
+		strArr = outputString.split("\\s+");
+		//currentJob = strArr[0];
 		currentJob = "NONE";
 	}
-	System.out.println("outside if statements");
+	
+	
+	
+	//System.out.println("outside if statements");
 }
 System.out.println("outside while loop");
 
